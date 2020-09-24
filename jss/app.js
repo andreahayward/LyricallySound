25ebe38147f818d0dbf01b6c681f047bbacd6cbd
Andrea Hayward <andrea.teofilo@yahoo.com>
2020-09-24 14:52:17 -0400

    additional html and API updates

--- jss/app.js
+++ jss/app.js
@@ -40,7 +40,24 @@ $(".button").click(function() {
     }
     localStorage.setItem("searchHistory", searchHistory)
 
-    //api call - put in function (event listener) move this to searchbutton.click function. take search var q= + searchvar. make the URL dynamic - pass them the dynamic URL
+//api call - put in function (event listener) move this to searchbutton.click function. take search var q= + searchvar. make the URL dynamic - pass them the dynamic URL
+//this works
+var genius = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://genius.p.rapidapi.com/search?q=" + search,
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "genius.p.rapidapi.com",
+		"x-rapidapi-key": "bdd1f87bcdmshb2ad5b0221de40cp1060d5jsn3d7c45fb4c54"
+	}
+}
+
+$.ajax(genius).done(function (response) {
+	console.log(response);
+});
+
+
 var settings = {
 	"async": true,
 	"crossDomain": true,
@@ -49,12 +66,21 @@ var settings = {
 	"headers": {
 		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
         "x-rapidapi-key": "bdd1f87bcdmshb2ad5b0221de40cp1060d5jsn3d7c45fb4c54"
-	}
+    }
 }
 
 $.ajax(settings).done(function (response) {
     console.log(response);
+    console.log(response.data[0].title);
+    // should add a for loop for the below
+    $(".titleone").text("1) " + response.data[0].title);
+    $(".titletwo").text("2) " + response.data[1].title);
+    $(".titlethree").text("3) " + response.data[2].title);
+    $(".titlefour").text("4) " + response.data[3].title);
+    $(".titlefive").text("5) " + response.data[4].title);
+
 });
+
 var musicSearch = $(".list-group").addClass("list-group-item");
         musicSearch.append("<li>" + search + "</li>");
 });
